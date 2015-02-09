1b13e0dac568074634e006e943d3ee95be937993
johnthedesigner <john@johnthedesigner.com>
2015-02-09 09:15:07 -0500

    setup angular with yeoman generator

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name scorecardAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the scorecardAppApp
+ */
+angular.module('scorecardAppApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
